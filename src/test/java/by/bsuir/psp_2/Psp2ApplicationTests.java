61005ac8373cbe045c98d49e50a9ab2095b8849b
Vladislav Karpeka <Vlad.Karpeka@vertexinc.com>
2022-01-07 15:56:45 +0300

    init commit

---
+++ src/test/java/by/bsuir/psp_2/Psp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.psp_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
